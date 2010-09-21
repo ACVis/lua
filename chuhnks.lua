48f9468a7a6ed003890610d25a6783a53756b4ef
Asim Aslam <asim@Macintosh.local>
2010-09-21 23:47:08 +0100

    chuhnks lua

--- chuhnks.lua
+++ chuhnks.lua
@@ -10,3 +10,8 @@ b = a*2;
 a = 1 ; b = a*2
 
 a = 1   b = a*2
+
+-- oh cool, chunks can be seperated into different files then called on the command line 
+-- with lua -i -l[file1] -l[file2]. file1 can contain x = 1 and file2 print(x).
+-- -l is the equivalent of require.
+

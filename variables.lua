28e0ebe3f03e3f1d4ef2d4556dcd0ea3a37a5b7a
Asim Aslam <asim@Macintosh.local>
2010-09-22 00:11:04 +0100

    variables lua

---
+++ variables.lua
@@ -0,0 +1,8 @@
+-- whoa lua vars rock
+print(type(a))   --> nil   (`a' is not initialized)
+a = 10
+print(type(a))   --> number
+a = "a string!!"
+print(type(a))   --> string
+a = print        -- yes, this is valid!
+a(type(a))       --> function

37ee2e1a15884ce1d1da0b1492805fb0bde17f25
Asim <asim@chuhnk.me>
2010-09-22 12:59:22 +0100

    relational lua

---
+++ relational.lua
@@ -0,0 +1,15 @@
+a = {}; a.x = 1; a.y = 0
+b = {}; b.x = 1; b.y = 0
+c = a
+
+if a == c then
+  print("a is equal to c")
+end
+
+if a ~= b then
+  print("a is not equal to b")
+end
+
+if ("0" == 0) == false then
+  print("this is false")
+end

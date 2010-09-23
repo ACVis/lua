8a325cdcd492cbfd96ea3ebc49a8ffae21672aaa
Asim Aslam <asim@macintosh.config>
2010-09-23 01:09:08 +0100

    concatenation lua

---
+++ concatenation.lua
@@ -0,0 +1,6 @@
+-- concatenation in lua converts numbers into strings.
+a = "Hello"
+print(a .. " World") --> Hello World, new string, immutable values
+print(1 .. 0) --> 10
+print("foo" .. "bar") --> foobar
+
